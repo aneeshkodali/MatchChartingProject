f19d68666f3c94aac4a78c6da89738ba3ae81004
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-06-30 08:03:55 -0400

    Connec to MongoDB (I think)

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const Player = require("./models/player");
 
 
 
@@ -18,7 +19,10 @@ app.set("view engine", "ejs");
 // ================
 // DB CONFIG
 // ================
-mongoose.connect('mongodb://localhost/matchChartingProject', {useNewUrlParser: true, useUnifiedTopology: true});
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mcp";
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log("CONNECTED TO DB"))
+.catch(err => console.log(err));
 
 
 
@@ -30,7 +34,7 @@ mongoose.connect('mongodb://localhost/matchChartingProject', {useNewUrlParser: t
 
 // root
 app.get("/", function(req, res) {
-    res.render("index");
+    res.render("home");
 });
 
 
@@ -39,3 +43,53 @@ app.listen(port, function() {
     console.log("Server now running");
 })
 
+
+// ================
+// SCRAPING
+// ================
+
+//const axios = require("axios");
+//const cheerio = require("cheerio");
+
+// 1) get to link of all players: http://www.minorleaguesplits.com/tennisabstract/cgi-bin/frags/
+    // check links against DB
+// 2) get to link of one player: http://www.minorleaguesplits.com/tennisabstract/cgi-bin/frags/RogerFederer.js
+// 3) get to tennisabstract link of that player: http://www.tennisabstract.com/cgi-bin/player-classic.cgi?p=RogerFederer
+// 4) get variables
+// 5) push to DB
+
+//const link = "http://www.minorleaguesplits.com/tennisabstract/cgi-bin/frags/"
+//const playerLink = "http://www.minorleaguesplits.com/tennisabstract/cgi-bin/frags/RogerFederer.js"
+//const playerLinkTA = "http://www.tennisabstract.com/cgi-bin/player-classic.cgi?p=RogerFederer"
+
+//const scrape = function(link="http://www.minorleaguesplits.com/tennisabstract/cgi-bin/frags/") {
+//  const playerLinks = [];
+//  return axios.get(link).then(function(response) {
+
+//    const $ = cheerio.load(response.data);
+
+//    const trArr = $("table tr").each(function(i, elem) {
+//      let playerLink = $(this).find('a').attr('href');
+//      if (playerLink && playerLink.endsWith(".js")) {
+//        playerLinks.push(link+playerLink);
+//      }
+//    });
+//    return playerLinks;
+//  });
+//}
+
+
+//const link_ta = "http://www.tennisabstract.com/cgi-bin/player-classic.cgi?p=RogerFederer";
+
+//const getTAData = function(link="http://www.tennisabstract.com/cgi-bin/player-classic.cgi?p=RogerFederer") {
+
+//  return axios.get(link).then(function(response) {
+//    const $ = cheerio.load(response.data);
+
+//    const script = $('script').filter(function(i, elem) {
+//      return $(this).attr("language") === "JavaScript";
+//    });
+//  })
+//}
+
+//console.log(getTAData());
